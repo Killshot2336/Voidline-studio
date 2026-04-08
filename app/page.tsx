a0885ecae3b431f01c37a18cbc9170fd7ee03dcf
Killshot2336 <Minerpro2026@gmail.com>
2026-04-08 15:35:36 -0500

    Delete app/page.tsx

--- app/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home() {
-  return (
-    <main style={{ padding: "40px", color: "white", background: "black", minHeight: "100vh" }}>
-      <h1>VOIDLINE TEST</h1>
-      <p>If this deploys, the problem is inside your real page/components code.</p>
-    </main>
-  );
-}
